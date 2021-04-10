11a733a702a3cdc0f6bfbd98f5409d5de1fb37d0
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-10 15:42:24 +0200

    fine

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 
+    //DROPDOWN
+    var dropDown = $('.drop-down');
+    var link = dropDown.children('li');
+    var tendina = dropDown.children('.drop-down-menu');
+
+    link.click(function(e){
+      //$(this).next('.drop-down-menu').toggle();
+
+      var menuCliccato = $('this').next('.drop-down-menu');
+
+      tendina.not(menuCliccato).hide();
+
+      menuCliccato.toggle();
+      
+    });
 
 
+ 
 
 
     //FINE READY
